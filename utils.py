67064bb157a96b16cb8e4fbf36f75147050cb5a3
aleyhany9 <aleyhany9@gmail.com>
2025-08-16 13:51:17 +0300

    add the colors

--- utils.py
+++ utils.py
@@ -27,3 +27,17 @@ class C:
     M = "\033[35m"
     GR = "\033[90m"
     RS = "\033[0m"
+def colorize(s, col):
+    if sys.stdout.isatty():
+        return f"{col}{s}{C.RS}"
+    return s
+
+WORD_LIST = """
+time year people way day man thing woman life child world school state family student group country problem hand part place case week company system program question work night point home water room mother area money story fact month lot right study book eye job word business issue side kind head house service friend father power hour game line end member law car city community name president team minute idea kid body information back parent face others level office door health person art war history party result change morning reason research girl guy moment air teacher force education foot boy age policy process music market sense service data window river machine science library planet galaxy python terminal battle rhythm keyboard accuracy progress bonus streak master champion victory
+""".split()
+
+def pick_word(min_len, max_len):
+    candidates = [w for w in WORD_LIST if min_len <= len(w) <= max_len]
+    if not candidates:
+        candidates = WORD_LIST[:]
+    return random.choice(candidates)
